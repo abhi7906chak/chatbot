89fb8b8e2fed3ab3f6dcd7df755df98c5d079368
abhi7906chak <abhi7906235584@gmail.com>
2024-12-30 20:05:52 +0530

    all done (mic button bug not done )

--- lib/secret.dart
+++
@@ -1,5 +0,0 @@
-const APIKEY =
-    "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";
-const APIKEY2 =
-    "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";
-const BARDAPI = "AIzaSyCf0uNTdn38AuLWXXEp2h3BypKBAkIlmmI";
